74f6684a390ccbc466eda96e4650d9b6810a4976
Andre Sharpe <andre.sharpe@gmail.com>
2023-03-12 15:52:18 +0100

    Initial Commit

---
+++ src/Nox.Reference.PhoneNumbers/Extensions/PhoneNumberExtensions.cs
@@ -0,0 +1,75 @@
+﻿using LibPhoneNumber = PhoneNumbers;
+
+namespace Nox.Reference.PhoneNumbers;
+
+public static partial class PhoneNumberExtensions
+{
+    private static readonly LibPhoneNumber.PhoneNumberUtil _phoneUtil = LibPhoneNumber.PhoneNumberUtil.GetInstance();
+
+    private static readonly LibPhoneNumber.PhoneNumberOfflineGeocoder _geoCoder = LibPhoneNumber.PhoneNumberOfflineGeocoder.GetInstance();
+
+    public static IPhoneNumberInfo GetPhoneNumberInfo(this string inputNumber, string? region = null)
+    {
+        var phoneNumber = _phoneUtil.Parse(inputNumber, region);
+
+        PhoneNumberInfo phoneInfo = new()
+        {
+            InputNumber = inputNumber,
+            IsValid = _phoneUtil.IsValidNumber(phoneNumber)
+        };
+
+        if (phoneInfo.IsValid)
+        {
+            phoneInfo.FormattedNumber = _phoneUtil.Format(phoneNumber, LibPhoneNumber.PhoneNumberFormat.E164);
+
+            phoneInfo.FormattedNumberNational = _phoneUtil.Format(phoneNumber, LibPhoneNumber.PhoneNumberFormat.NATIONAL);
+
+            phoneInfo.FormattedNumberInternational = _phoneUtil.Format(phoneNumber, LibPhoneNumber.PhoneNumberFormat.INTERNATIONAL);
+
+            phoneInfo.FormattedNumberRfc3966 = _phoneUtil.Format(phoneNumber, LibPhoneNumber.PhoneNumberFormat.RFC3966);
+
+            phoneInfo.RegionCode = _phoneUtil.GetRegionCodeForNumber(phoneNumber);
+
+            phoneInfo.NumberType = _phoneUtil.GetNumberType(phoneNumber).ToString() ?? "";
+
+            phoneInfo.IsMobile = !string.IsNullOrEmpty(phoneInfo.NumberType) && phoneInfo.NumberType.Equals("MOBILE");
+
+            phoneInfo.RegionName= _geoCoder.GetDescriptionForNumber(phoneNumber, LibPhoneNumber.Locale.English);
+
+            phoneInfo.CarrierName = phoneInfo.FormattedNumber.GuessCarrier();
+        }
+        return phoneInfo;
+    }
+
+    private static string GuessCarrier(this string formattedNumber)
+    {
+        var key = Convert.ToInt32(formattedNumber.TrimStart('+').PadRight(9,'0')[..9]);
+
+        int min = 0;
+        int max = _carrierMap.Length - 1;
+        int mid;
+        while (min <= max)
+        {
+            mid = (min + max) / 2;
+            if (key == _carrierMap[mid,0])
+            {
+                max = mid;
+                break;
+            }
+            else if (key < _carrierMap[mid,0])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+
+        if (min < 1 || max > _carrierMap.Length-1) return "Unknown";
+     
+        return _carriers[_carrierMap[max, 1]];
+
+    }
+}
+
---
+++ src/Nox.Reference.PhoneNumbers/README.md
@@ -0,0 +1,41 @@
+# Nox.Utilities.PhoneNumberInfo
+Validate and format phone numbers
+
+```csharp
+using System.Text.Json;
+using Nox.Reference.PhoneNumbers;
+
+IPhoneNumberInfo info = "833770694".GetPhoneNumberInfo("ZA"); 
+
+var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
+Console.WriteLine(JsonSerializer.Serialize(info,options));
+
+/* Outputs:
+{
+  "inputNumber": "833770694",
+  "formattedNumber": "\u002B27833770694",
+  "formattedNumberNational": "083 377 0694",
+  "formattedNumberInternational": "\u002B27 83 377 0694",
+  "formattedNumberRfc3966": "tel:\u002B27-83-377-0694",
+  "isValid": true,
+  "isMobile": true,
+  "numberType": "MOBILE",
+  "regionCode": "ZA",
+  "regionName": "South Africa",
+  "carrierName": "MTN"
+}
+*/
+```
+
+### To install from nuget.org
+```powershell
+dotnet add package Nox.Utilities.PhoneNumberInfo
+```
+
+### Dependancies
+Uses Thomas Clegg's [libphonenumber-csharp](https://github.com/twcclegg/libphonenumber-csharp) which is a C# port of [Google's libphonenumber](https://github.com/google/libphonenumber) Java project.
