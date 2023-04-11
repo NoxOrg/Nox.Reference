3c28eaaefe3a4b40914967d25cf44dbbec13516e
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-04-11 16:12:14 +0300

    fix nullable

--- src/Nox.Reference.Abstractions/VatNumbers/ViesApproximate.cs
+++ src/Nox.Reference.Abstractions/VatNumbers/ViesApproximate.cs
@@ -2,11 +2,11 @@
 {
     public class ViesApproximate
     {
-        public string name { get; set; }
-        public string street { get; set; }
-        public string postalCode { get; set; }
-        public string city { get; set; }
-        public string companyType { get; set; }
+        public string? name { get; set; }
+        public string? street { get; set; }
+        public string? postalCode { get; set; }
+        public string? city { get; set; }
+        public string? companyType { get; set; }
         public int matchName { get; set; }
         public int matchStreet { get; set; }
         public int matchPostalCode { get; set; }
--- src/Nox.Reference.Abstractions/VatNumbers/ViesVerificationResponse.cs
+++ src/Nox.Reference.Abstractions/VatNumbers/ViesVerificationResponse.cs
@@ -4,11 +4,11 @@
     {
         public bool isValid { get; set; }
         public DateTime requestDate { get; set; }
-        public string userError { get; set; }
-        public string name { get; set; }
-        public string address { get; set; }
-        public string requestIdentifier { get; set; }
-        public string vatNumber { get; set; }
-        public ViesApproximate viesApproximate { get; set; }
+        public string? userError { get; set; }
+        public string? name { get; set; }
+        public string? address { get; set; }
+        public string? requestIdentifier { get; set; }
+        public string? vatNumber { get; set; }
+        public ViesApproximate? viesApproximate { get; set; }
     }
 }
