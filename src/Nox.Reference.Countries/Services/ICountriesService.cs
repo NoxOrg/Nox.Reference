5c4629584e5cee59242fe8d4f6cf17ab6dcad2c1
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-04-13 11:34:30 +0300

    document interfaces

--- src/Nox.Reference.Countries/Services/ICountriesService.cs
+++ src/Nox.Reference.Countries/Services/ICountriesService.cs
@@ -2,6 +2,10 @@
 {
     public interface ICountriesService
     {
+        /// <summary>
+        /// Get countries list
+        /// </summary>
+        /// <returns>IReadOnlyList with all supported countries and their info</returns>
         IReadOnlyList<ICountryInfo> GetCountries();
     }
 }
--- src/Nox.Reference.VatNumbers/Services/IVatNumberService.cs
+++ src/Nox.Reference.VatNumbers/Services/IVatNumberService.cs
@@ -4,7 +4,19 @@ namespace Nox.Reference.Holidays
 {
     public interface IVatNumberService
     {
+        /// <summary>
+        /// Get VAT number validation info for a particular country
+        /// </summary>
+        /// <param name="vatNumberInfo">VAT Number object with just a country or with country and number specified</param>
+        /// <returns>Vat number validation info</returns>
         IVatNumberInfo GetCountryVatInfo(IVatNumberInfo vatNumberInfo);
+
+        /// <summary>
+        /// Get VAT number validation info and validate it for a particular country
+        /// </summary>
+        /// <param name="vatNumberInfo">VAT Number with country and number specified</param>
+        /// <param name="shouldValidateViaApi">A flag whether API validation should be used if applicable</param>
+        /// <returns>Validation Vat number info</returns>
         IVatNumberInfo ValidateVatNumber(IVatNumberInfo vatNumberInfo, bool shouldValidateViaApi = true);
     }
 }
