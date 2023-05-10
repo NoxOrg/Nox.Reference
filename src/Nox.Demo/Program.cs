c66059aa5739ef3706e35574b54353fc44d075ac
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-05-10 17:57:30 +0300

    Fix build

---
+++ src/Nox.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nox.Reference.Data;
+using Nox.Reference.Data.World;
+using Nox.Reference.Data.World.Extensions.Queries;
+
+Console.WriteLine("This is Nox.Reference Demo!");
+
+// Simplified
+
+var context2 = WorldDataContext.Create();
+var timezone = context2.TimeZones.Get("EET")!;
+
+Console.WriteLine($"{timezone.Id} -- {timezone.Type}");
+
+// From DI
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddWorldContext();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var context = serviceProvider.GetRequiredService<IWorldInfoContext>();
+timezone = context.TimeZones.Get("EET")!;
+
+Console.WriteLine($"{timezone.Id} -- {timezone.Type}");
