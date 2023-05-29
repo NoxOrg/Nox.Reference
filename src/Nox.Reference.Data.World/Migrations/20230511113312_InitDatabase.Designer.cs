8e88c742b795b42c09a13a0d5683fd2f1982afe4
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-05-29 13:38:16 +0300

    update data seed

--- src/Nox.Reference.Data.World/Migrations/20230511113312_InitDatabase.Designer.cs
+++ src/Nox.Reference.Data.World/Migrations/20230511113312_InitDatabase.Designer.cs
@@ -18,7 +18,11 @@ namespace Nox.Reference.Data.World.Migrations
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "7.0.4");
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.4")
+                .HasAnnotation("Proxies:ChangeTracking", false)
+                .HasAnnotation("Proxies:CheckEquality", false)
+                .HasAnnotation("Proxies:LazyLoading", true);
 
             modelBuilder.Entity("ContinentCountry", b =>
                 {
