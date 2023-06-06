c93c13cb82f22cba475ec585333b6ec714292af9
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-06-06 18:08:50 +0300

    remove-space

--- src/Nox.Reference.Data.World/Entities/Countries/CoatOfArms.cs
+++ src/Nox.Reference.Data.World/Entities/Countries/CoatOfArms.cs
@@ -6,6 +6,5 @@ public class CoatOfArms : INoxReferenceEntity
 {
     public int Id { get; private set; }
     public string Svg { get; private set; } = string.Empty;
-
     public string Png { get; private set; } = string.Empty;
 }
