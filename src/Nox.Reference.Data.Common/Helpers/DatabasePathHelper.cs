66aec38e96129cdb07a9d57f7c0babb9399102f2
Dejan Bratic <dejan.bratic@igwplc.com>
2023-12-26 10:24:39 +0100

    added path fix;

--- src/Nox.Reference.Data.Common/Helpers/DatabasePathHelper.cs
+++ src/Nox.Reference.Data.Common/Helpers/DatabasePathHelper.cs
@@ -7,10 +7,13 @@ namespace Nox.Reference.Data.Common.Helpers;
 /// </summary>
 internal static class DatabasePathHelper
 {
+    private static readonly char FileSeparator = Path.DirectorySeparatorChar;
+
     /// <summary>
     /// This method is used to fix database file detection. For different types of applications
-    /// Directory.GetCurrentDirectory returns different values, which leads to file not found errors.
-    /// This method checks if the default relative path is working and fixes it according to assembly location
+    /// Directory.GetCurrentDirectory returns different values, which leads to file not found
+    /// errors. This method checks if the default relative path is working and fixes it according to
+    /// assembly location.
     /// </summary>
     /// <param name="connectionString">Provided connection string to check</param>
     /// <param name="databaseContextType">Database context type</param>
@@ -20,35 +23,54 @@ internal static class DatabasePathHelper
     /// <exception cref="Exception"></exception>
     public static string FixConnectionStringPathUsingAssemblyPath(string? connectionString, Type databaseContextType, string contextName)
     {
+        ThrowIfConnectionStringIsNotDefined(connectionString, contextName);
+
+        if (CheckIfFileInConnectionStringExists(connectionString))
+            return connectionString!;
+
+        return GetFixedConnectionString(connectionString, databaseContextType, contextName);
+    }
+
+    private static void ThrowIfConnectionStringIsNotDefined(string? connectionString, string contextName)
+    {
         if (string.IsNullOrWhiteSpace(connectionString))
         {
             var errorMessage = $"{contextName} database file path is null";
             throw new ArgumentNullException(errorMessage);
         }
-        var splitDbPath = connectionString.Split("=");
-        var filePath = splitDbPath[1];
+    }
 
-        if (File.Exists(filePath))
-        {
-            return connectionString;
-        }
+    private static bool CheckIfFileInConnectionStringExists(string? connectionString)
+    {
+        var filePath = connectionString!.Split("=")[1];
+        return File.Exists(filePath);
+    }
 
+    private static string GetFixedConnectionString(string? connectionString, Type databaseContextType, string contextName)
+    {
         var currentAssemblyLocation = Assembly.GetAssembly(databaseContextType)!.Location;
         var currentAssemblyDirectory = Path.GetDirectoryName(currentAssemblyLocation);
 
+        var fixedConnectionString = Path.Combine(connectionString!.Split(separator: '\\', '/'));
+
         // resolve . operator
-        var fixedConnectionString = connectionString.Replace("=.\\", $"={currentAssemblyDirectory}\\");
+        fixedConnectionString = fixedConnectionString!.Replace($"=.{FileSeparator}", $"={currentAssemblyDirectory}{FileSeparator}");
 
         // resolve .. operator
-        fixedConnectionString = fixedConnectionString.Replace("=..\\", $"={currentAssemblyDirectory}\\..\\");
+        fixedConnectionString = fixedConnectionString.Replace($"=..{FileSeparator}", $"={currentAssemblyDirectory}{FileSeparator}..{FileSeparator}");
 
-        filePath = fixedConnectionString.Split("=")[1];
+        var filePath = fixedConnectionString.Split("=")[1];
+        ThrowIfFileInConnectionStringDoesNotExist(connectionString, contextName, filePath);
+
+        return fixedConnectionString;
+    }
+
+    private static void ThrowIfFileInConnectionStringDoesNotExist(string? connectionString, string contextName, string filePath)
+    {
         if (!File.Exists(filePath))
         {
             var errorMessage = $"Cannot find {contextName} database by the provided connection string {connectionString}. Please, check the path or override it using {contextName}.UseDatabasePath(path). Possibly an issue with '.\\' operator is present.";
             throw new FileNotFoundException(errorMessage);
         }
-
-        return fixedConnectionString;
     }
 }
