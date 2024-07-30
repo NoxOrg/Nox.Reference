f7460f419db9bb9db082dfa006a74e39f99349f2
Dejan Bratic <dejan.bratic@igwplc.com>
2024-07-30 11:58:23 +0200

    updated to .net8;

--- src/Nox.Reference.Data.World/Exceptions/HolidayRegionNotFoundException.cs
+++ src/Nox.Reference.Data.World/Exceptions/HolidayRegionNotFoundException.cs
@@ -1,28 +1,17 @@
-﻿using System.Runtime.Serialization;
+﻿namespace Nox.Reference.Data.World.Exceptions;
 
-namespace Nox.Reference.Data.World.Exceptions
+public class HolidayRegionNotFoundException : Exception
 {
-
-    [Serializable]
-    public class HolidayRegionNotFoundException : Exception
+    public HolidayRegionNotFoundException() : base("Holiday region not found.")
     {
-        public HolidayRegionNotFoundException() : base("Holiday region not found.")
-        {
-        }
+    }
 
-        public HolidayRegionNotFoundException(string message) : base(message)
-        {
-        }
+    public HolidayRegionNotFoundException(string message) : base(message)
+    {
+    }
 
-        public HolidayRegionNotFoundException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-
-        // Without this constructor, deserialization would fail
-        protected HolidayRegionNotFoundException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
+    public HolidayRegionNotFoundException(string message, Exception innerException)
+        : base(message, innerException)
+    {
     }
 }
